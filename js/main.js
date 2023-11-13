1149df91229b358cb762a45bb676e85f0105d703
Marco <marcoregina92@gmail.com>
2023-11-13 20:11:39 +0100

    Aggiunge note

--- js/main.js
+++ js/main.js
@@ -1,42 +1,48 @@
-// const scheda = [
-//     {
-//         image: 'img/01.webp',
-//         title: 'Marvel\'s Spiderman Miles Morale',
-//         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-//     }, {
-//         image: 'img/02.webp',
-//         title: 'Ratchet & Clank: Rift Apart',
-//         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-//     }, {
-//         image: 'img/03.webp',
-//         title: 'Fortnite',
-//         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-//     }, {
-//         image: 'img/04.webp',
-//         title: 'Stray',
-//         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-//     }, {
-//         image: 'img/05.webp',
-//         title: "Marvel's Avengers",
-//         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-//     }
-// ];
-
-// let imagesToAdd = "";
-
-// scheda.forEach((element, index) => {
-
-//     console.log(element, index);
-//     let image = `<img src="${element.image}">`;
-//     imagesToAdd += image;
-// });
-
-// document.getElementById("boxImage").innerHTML = imagesToAdd;
+const scheda = [
+    {
+        image: 'img/01.webp',
+        title: 'Marvel\'s Spiderman Miles Morale',
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+    }, {
+        image: 'img/02.webp',
+        title: 'Ratchet & Clank: Rift Apart',
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+    }, {
+        image: 'img/03.webp',
+        title: 'Fortnite',
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+    }, {
+        image: 'img/04.webp',
+        title: 'Stray',
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+    }, {
+        image: 'img/05.webp',
+        title: "Marvel's Avengers",
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    }
+];
 
 
-// let currentImg = 0;
-// const images = document.querySelectorAll("#boxImage img");
-// images[currentImg].classList.add("blockImage");
+// Ciclo per inserire immagini
+let imagesToAdd = "";
+
+scheda.forEach((element, index) => {
+
+    console.log(element, index);
+    let image = `<img src="${element.image}">`;
+    imagesToAdd += image;
+});
+
+// Stampo le immagini in DOM
+document.getElementById("boxImage").innerHTML = imagesToAdd;
+
+// Inserisco la prima immagina di default
+let currentImg = 0;
+const images = document.querySelectorAll("#boxImage img");
+images[currentImg].classList.add("blockImage");
+
+
+
 
 // document.getElementById("btnRight").addEventListener("click", function () {
 //     console.log("avanti");
